23f991d22ece3f4da40cfa5ead6177e670e214e2
Ogie Galicia <o.galicia.cpa@gmail.com>
2024-05-27 19:33:20 +0800

    fix(vaccine): update layout

--- src/routes/(home)/vaccines/[id]/+page.svelte
+++ src/routes/(home)/vaccines/[id]/+page.svelte
@@ -6,7 +6,7 @@
 	const formData = data.form!;
 </script>
 
-<h1 class="mb-4 text-3xl">Edit Vaccine</h1>
 <div class="p-0 sm:rounded-xl sm:bg-slate-500/10 sm:p-8">
+	<h1 class="mb-4 text-3xl">Edit Vaccine</h1>
 	<Form data={formData} editing></Form>
 </div>
--- src/routes/(home)/vaccines/add/+page.svelte
+++ src/routes/(home)/vaccines/add/+page.svelte
@@ -4,7 +4,7 @@
 	export let data: PageData;
 </script>
 
-<h1 class="mb-4 text-3xl">New Vaccine</h1>
 <div class="p-0 sm:rounded-xl sm:bg-slate-500/10 sm:p-8">
+	<h1 class="mb-4 text-3xl">New Vaccine</h1>
 	<Form data={data.form}></Form>
 </div>

ff741db3f259b59f57e0a830cc496ec1e9932056
Ogie Galicia <o.galicia.cpa@gmail.com>
2024-05-27 21:26:48 +0800

    feat: implement animal edit

---
+++ src/routes/(home)/animals/[id]/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import Form from '../form.svelte';
+	export let data: PageData;
+
+	const formData = data.form!;
+</script>
+
+<div class="p-0 sm:rounded-xl sm:bg-slate-500/10 sm:p-8">
+	<h1 class="mb-4 text-3xl">Edit Animal</h1>
+	<Form data={formData} editing></Form>
+</div>

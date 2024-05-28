129663fa308201816ae45f22593671fb6dafd29b
Ogie Galicia <o.galicia.cpa@gmail.com>
2024-05-28 08:48:20 +0800

    fix(animal): ensure image is also deleted

--- src/routes/api/animals/[id]/+server.ts
+++ src/routes/api/animals/[id]/+server.ts
@@ -1,8 +1,9 @@
 import { lucia } from '$lib/server/auth';
 import { db } from '$lib/server/db';
 import { animal } from '$lib/server/db/schema';
+import { cloudinary } from '$lib/upload/cloudinary';
 import { error, json } from '@sveltejs/kit';
-import { eq } from 'drizzle-orm';
+import { and, eq } from 'drizzle-orm';
 
 export async function DELETE({ cookies, params }) {
 	const sessionId = cookies.get(lucia.sessionCookieName);
@@ -16,7 +17,21 @@ export async function DELETE({ cookies, params }) {
 		error(401, 'Unauthorized');
 	}
 
+	const current = await db.query.animal.findFirst({
+		where: and(eq(animal.id, params.id), eq(animal.userId, user.id))
+	});
+
+	if (!current) {
+		error(404, 'Animal not found');
+	}
+
 	await db.delete(animal).where(eq(animal.id, params.id));
+
+	if (current.cloudinaryId) {
+		cloudinary.uploader.destroy(current.cloudinaryId, function () {
+			console.log('Deleted image', current.cloudinaryId);
+		});
+	}
 
 	return json({ success: true, id: params.id });
 }

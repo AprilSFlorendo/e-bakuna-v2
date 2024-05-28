7a50901793a3a5bc23571958403081268e61bd76
Ogie Galicia <o.galicia.cpa@gmail.com>
2024-05-28 10:00:22 +0800

    fix: update animal list title

--- src/routes/(home)/animals/+page.svelte
+++ src/routes/(home)/animals/+page.svelte
@@ -74,7 +74,7 @@
 />
 <div class="flex flex-col gap-4 p-0 sm:rounded-xl sm:bg-slate-500/10 sm:p-8">
 	<div class="flex items-center justify-between">
-		<h1 class="text-3xl">animals</h1>
+		<h1 class="text-3xl">Animals</h1>
 		<Button class="self-end" href="animals/add">Add New</Button>
 	</div>
 	<Table.Root>

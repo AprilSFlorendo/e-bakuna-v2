248273159e64afbc9959d183069671eb90833e02
Ogie Galicia <o.galicia.cpa@gmail.com>
2024-05-28 09:55:03 +0800

    fix: remove github login

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,14 +1,10 @@
-import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
 import { DrizzleSQLiteAdapter } from '@lucia-auth/adapter-drizzle';
 import { redirect } from '@sveltejs/kit';
-import { GitHub } from 'arctic';
 import { Lucia } from 'lucia';
 import { db } from './db';
 import { session, user } from './db/schema/user';
 
 const adapter = new DrizzleSQLiteAdapter(db, session, user);
-
-export const github = new GitHub(GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET);
 
 export const lucia = new Lucia(adapter, {
 	sessionCookie: {

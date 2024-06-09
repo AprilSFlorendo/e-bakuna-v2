ee64dad28cab6641a2665d1206d37ecbdff86757
Ogie Galicia <o.galicia.cpa@gmail.com>
2024-06-09 15:39:11 +0800

    fix: not logging out

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,33 +2,31 @@ import { lucia } from '$lib/server/auth';
 import { error, redirect, type Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
+	const path = event.url.pathname;
+	if (path.startsWith('/login') || path.startsWith('/logout')) {
+		return resolve(event);
+	}
+
 	const sessionId = event.cookies.get(lucia.sessionCookieName);
 	if (!sessionId) {
 		event.locals.user = null;
 		event.locals.session = null;
 
-		if (event.url.pathname.includes('/api')) {
-			error(401, 'Unauthorized');
-		}
-
-		if (event.url.pathname.includes('/login') || event.url.pathname.includes('/register')) {
-			return resolve(event);
-		}
-
-		return redirect(303, '/login');
+		return redirect(302, '/login');
 	}
 
 	const { session, user } = await lucia.validateSession(sessionId);
-	if (event.url.pathname.includes('/api') && !user) {
-		error(401, 'Unauthorized');
+
+	if (!user) {
+		redirect(302, '/login');
 	}
 
-	if (user?.isAdmin && !event.url.pathname.startsWith('/admin')) {
-		return redirect(303, '/admin');
+	if (user && user.isAdmin && !event.url.pathname.startsWith('/admin')) {
+		redirect(302, '/admin');
 	}
 
-	if (!user?.isAdmin && event.url.pathname.startsWith('/admin')) {
-		return error(401, 'Sorry, you are not authorized to view this page.');
+	if (user && !user.isAdmin && event.url.pathname.startsWith('/admin')) {
+		error(403, 'Sorry, you do not have permission to access this page.');
 	}
 
 	if (session && session.fresh) {
@@ -48,5 +46,6 @@ export const handle: Handle = async ({ event, resolve }) => {
 
 	event.locals.user = user;
 	event.locals.session = session;
+
 	return resolve(event);
 };
